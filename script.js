0ca216a4a1ced74bdf38c29d164ccc9c4dcc1dd7
SKYDONZAD <sambecky66@outlook.com>
2025-04-11 01:33:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("clickMe").addEventListener("click", function() {
+    alert("Button clicked! You're learning to code.");
+});
+document.getElementById("clickMe").addEventListener("click", function() {
+    this.God bless you = "Thank you for clicking!";
+});
